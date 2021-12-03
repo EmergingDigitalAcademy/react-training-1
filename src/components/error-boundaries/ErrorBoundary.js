fd81c60ac20ec20e6b4e4df49eb879904f1cff2d
Jaryd Krishnan <jaryd@hey.com>
2021-12-03 17:51:23 -0600

    Handle errors

--- src/components/error-boundaries/ErrorBoundary.js
+++ src/components/error-boundaries/ErrorBoundary.js
@@ -11,7 +11,28 @@ class ErrorBoundary extends Component {
     this.setState({ hasError: !this.state.hasError });
   }
 
+  componentDidCatch(error, info) {
+      this.setState({ hasError: true });
+  }
+
   render() {
+    if (this.state.hasError) {
+      return (
+        <div className="container">
+          <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">Error!</h1>
+              <p className="col-md-8 fs-4">
+                The child component has crashed.
+              </p>
+              <button className="btn btn-danger btn-lg" type="button" onClick={this.toggleError}>
+                {this.state.hasError ? 'Hide Error' : 'Trigger Error'}
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="container" id="errors">
         <ErrorBoundaryChild />
