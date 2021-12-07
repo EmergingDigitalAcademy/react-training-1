e157cbcd20364d56920627a2aa23663567e1a34e
Jaryd Krishnan <jaryd@hey.com>
2021-12-07 10:10:55 -0600

    Add error handling

--- src/components/error-boundaries/ErrorBoundary.js
+++ src/components/error-boundaries/ErrorBoundary.js
@@ -1,8 +1,39 @@
 import { Component } from 'react';
 import ErrorBoundaryChild from './ErrorBoundaryChild';
-
+import * as Sentry from "@sentry/react";
 class ErrorBoundary extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  componentDidCatch(error, info) {
+    Sentry.captureException(error);
+    this.setState({ hasError: true });
+  }
+
+  resetError() {
+    this.setState({ hasError: false });
+  }
+
   render() {
+    if (this.state.hasError) {
+      return (
+        <div className="container" id="errors">
+          <div className="p-5 mb-4 bg-light rounded-3">
+            <div className="container-fluid py-5">
+              <h1 className="display-5 fw-bold">You had an error</h1>
+              <p className="col-md-8 fs-4">
+                Jaryd is a big dummy, who wrote an erroring component.
+              </p>
+              <button className="btn btn-danger btn-lg" type="button" onClick={() => this.setState({ hasError: false })}>
+                Clear Error
+              </button>
+            </div>
+          </div>
+        </div>
+      );
+    }
     return (
       <div className="container" id="errors">
         <ErrorBoundaryChild />
--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import * as Sentry from "@sentry/react";
+import { Integrations } from "@sentry/tracing";
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+Sentry.init({
+  dsn: "https://49822e3429c544708b00ea7a26b5fbe3@o1083407.ingest.sentry.io/6097193",
+  integrations: [new Integrations.BrowserTracing()],
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
 
 ReactDOM.render(
   <React.StrictMode>
